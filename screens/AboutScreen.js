3ba1b162e27c2abb44a41eba91e1d35cf8d9a750
Tim Pedersen <drsteamup@gmail.com>
2023-11-27 22:27:57 -0600

    Setting Up Redux

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,13 +1,10 @@
-
-
 import React from 'react';
 import { useState } from 'react';
 import { ScrollView } from 'react-native';
 import { Card, Text, ListItem, Avatar } from 'react-native-elements';
-// import partners from '../shared/partners';
 import { PARTNERS } from '../shared/partners';
 
-// Mission component
+
 const Mission = () => {
   return (
     <Card>
@@ -20,21 +17,15 @@ const Mission = () => {
   );
 };
 
-// AboutScreen component
 const AboutScreen = () => {
   const [partnersData] = useState(PARTNERS);
 
   return (
     <ScrollView>
-      {/* Render the Mission component */}
       <Mission />
-
-      {/* Render the Community Partners Card */}
       <Card>
         <Card.Title>Community Partners</Card.Title>
         <Card.Divider />
-
-        {/* Use map to render ListItem for each partner */}
         {partnersData.map((partner) => (
           <ListItem key={partner.id}>
             <Avatar rounded source={partner.image} />
@@ -51,83 +42,4 @@ const AboutScreen = () => {
 
 export default AboutScreen;
 
-// AboutScreen.js
-// import React from 'react';
-// import { ScrollView } from 'react-native';
-
-// const AboutScreen = () => {
-//   return (
-//     <ScrollView>
-//       {/* Add content here in Task 2 */}
-//     </ScrollView>
-//   );
-// };
-
-// export default AboutScreen;
-
-
-
-// import React from 'react';
-// import { ScrollView } from 'react-native';
-
-// const AboutScreen = () => {
-//   return (
-//     <ScrollView>
-//       {/* Content for AboutScreen will be added in Task 2 */}
-//     </ScrollView>
-//   );
-// };
-
-//  export default AboutScreen;
-
-
-// import React from 'react' ;
-// import { useState } from 'react';
-// import { ScrollView } from 'react-native';
-// import { Card, Text, ListItem, Avatar } from 'react-native-elements';
-// import { PARTNERS } from '../shared/partners'; 
-
-
-// const Mission = () => {
-//   return (
-//     <Card>
-//       <Card.Title>Our Mission</Card.Title>
-//       <Card.Divider />
-//       <Text style={{ margin: 10 }}>
-//         We present a curated database of the best campsites in the vast woods and
-//         backcountry of the World Wide Web Wilderness. We increase access to adventure
-//         for the public while promoting safe and respectful use of resources. The expert
-//         wilderness trekkers on our staff personally verify each campsite to make sure
-//         that they are up to our standards. We also present a platform for campers to
-//         share reviews on campsites they have visited with each other.
-//       </Text>
-//     </Card>
-//   );
-// };
-
-// const AboutScreen = () => {
-//   const [partnersData] = useState(PARTNERS);
-
-//   return (
-//     <ScrollView>
-//       <Mission />
-//       <Card>
-//         <Card.Title>Community Partners</Card.Title>
-//         <Card.Divider />
-//         {partnersData.map((partner) => {
-//           <ListItem key={partner.id}>
-//             <Avatar rounded source={partner.image} />
-//             <ListItem.Content>
-//               <ListItem.Title>{partner.name}</ListItem.Title>
-//               <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
-//             </ListItem.Content>
-//           </ListItem>
-//           })}
-//       </Card>
-
-//     </ScrollView>
-//   );
-// };
-
-// export default AboutScreen;
 
